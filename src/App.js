a4953abe2d014b192ac04cf1dedebad0134a6460
matthewcuan <cuanmatthewl@gmail.com>
2023-03-21 14:35:24 -0700

    styled experience

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import Projects from './components/Projects';
 import Contact from './components/Contact'; 
+import Experience from './components/Experience';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Welcome />
       <Projects />
+      <Experience />
       <Contact />
     </div>
   );
