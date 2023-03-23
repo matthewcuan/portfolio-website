b524f997c1b733d5eed18131111edddfb8861b68
matthewcuan <cuanmatthewl@gmail.com>
2023-03-23 12:46:47 -0700

    styled dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,15 +58,16 @@ function Navigation() {
                   Projects
               </Nav.Link>
               <NavDropdown 
+                className="dropdown"
                 title="Projects" 
                 id="basic-nav-dropdown"
                 show={show}
                 onMouseEnter={showDropdown} 
                 onMouseLeave={hideDropdown}
               >
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Item href="/project">Project 1</NavDropdown.Item>
+                <NavDropdown.Item href="/project">Project 2</NavDropdown.Item>
+                <NavDropdown.Item href="/project">Project 3</NavDropdown.Item>
               </NavDropdown>
               <Nav.Link href="/#experience-section" className="nav-link underline">
                   Experience
