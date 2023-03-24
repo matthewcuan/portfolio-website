f8b984efbd1a5cf1e92d50d90c2924b8fc809e1a
matthewcuan <cuanmatthewl@gmail.com>
2023-03-23 21:16:17 -0700

    animated welcome and about sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,14 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 function About() {
   return (
     <div id="about-section">
-      <div class="bio">
+      <motion.aside 
+        initial={{ x: -200 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 0.5}}
+        className="bio">
         <h2>
           Welcome to my portfolio page!
         </h2>
@@ -18,10 +23,14 @@ function About() {
           morbi quis commodo. Nisi scelerisque eu ultrices vitae 
           auctor. Facilisis magna etiam tempor orci.
         </p>
-      </div>
-      <div class="photo">
-        <img src="https://cdn1.iconfinder.com/data/icons/conceptual-figures-3/48/bl_1627_stick_man_brush_bucket_painter_artist_social_media-512.png"></img>
-      </div>
+      </motion.aside>
+      <motion.aside 
+        initial={{ x: 200 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 0.5}}
+        className="photo">
+          <img src="https://cdn1.iconfinder.com/data/icons/conceptual-figures-3/48/bl_1627_stick_man_brush_bucket_painter_artist_social_media-512.png"></img>
+      </motion.aside>
     </div>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Welcome() {
   return (
     <section id="welcome-section">
       <div className="introduction">
+        <motion.aside
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{
+            duration: 2,
+          }}
+        >
         <h1>My name is Matthew Cuan</h1>
         <p>
           <i>uc berkeley alum and software developer</i>
         </p>
+        </motion.aside >
       </div>
     </section>
   );
